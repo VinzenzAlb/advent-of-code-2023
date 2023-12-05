80ff4ebaed6472b556b061b88fc0c0b4889c63ad
Vinzenz Alb <valb.mmt-b2022@fh-salzburg.ac.at>
2023-12-05 12:45:04 +0100

    finished and working - day 04

--- adventofcode04/Solution.cs
+++ adventofcode04/Solution.cs
@@ -55,14 +55,35 @@ namespace adventofcode2023
 
         public string SolutionOfSecondPart(string[] lines)
         {
-            int[] solution = initArray(lines.Length, 1);
+            int[] solution = initArray(lines.Length);
 
-            // Hier kommt die Logik für den zweiten Teil hin
+            for (int curentTicket = 0; curentTicket < lines.Length; curentTicket++)
+            {
+                int winCount = 0;
+                string input = lines[curentTicket].Split(":")[1];
+                List<int> win = getNumbers(input.Split("|")[0]);
+                List<int> myNum = getNumbers(input.Split("|")[1]);
+                foreach (int num in myNum)
+                {
+                    if (win.Contains(num)) winCount++;
+                }
+                for (int ticketCount = 0; ticketCount < solution[curentTicket]; ticketCount++)
+                {
+                    int winCounterThisTicket = winCount;
+                    int line = curentTicket;
+                    while (winCounterThisTicket > 0 && line + 1 < lines.Length)
+                    {
+                        solution[line + 1]++;
+                        winCounterThisTicket--;
+                        line++;
+                    }
+                }
+            }
 
-            return solution.Sum;
+            return solution.Sum().ToString();
         }
 
-        private int[] initArray(int length, int value)
+        private int[] initArray(int length, int value = 1)
         {
             int[] result = new int[length];
             for (int i = 0; i < length; i++)
